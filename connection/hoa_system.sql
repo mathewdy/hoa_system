a2779dd1fd638be8cb35664cad61db7a98d3a1fb
mathewdy <88813049+mathewdy@users.noreply.github.com>
2025-11-05 21:00:08 +0800

    updated sql

--- connection/hoa_system.sql
+++ connection/hoa_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2025 at 08:00 PM
+-- Generation Time: Nov 05, 2025 at 01:59 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `hoa_system`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fee_type`
+--
+
+CREATE TABLE `fee_type` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fee_name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `remarks` varchar(255) NOT NULL,
+  `approved` int(11) NOT NULL,
+  `datetime_created` datetime NOT NULL,
+  `datetime_updated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,6 +84,7 @@ CREATE TABLE `users` (
   `date_of_birth` date NOT NULL,
   `citizenship` varchar(100) NOT NULL,
   `civil_status` varchar(100) NOT NULL,
+  `account_status` int(11) NOT NULL,
   `home_address` varchar(255) DEFAULT NULL,
   `lot_number` int(11) NOT NULL,
   `block_number` int(11) NOT NULL,
@@ -78,19 +98,33 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `email_address`, `hoa_number`, `phone_number`, `age`, `date_of_birth`, `citizenship`, `civil_status`, `home_address`, `lot_number`, `block_number`, `phase_number`, `village_name`, `date_created`, `date_updated`) VALUES
-(16, 2, 20257108, 'Isabelle', 'Rebecca Briggs', 'Jones', '0', 'tywuly@mailinator.com', 750, '+639917137979', 63, '2003-05-17', 'Perferendis sunt sit', 'Annulled', 'Eum ea id saepe adi', 435, 557, 191, 'Hiram Hickman', '2025-11-05', '2025-11-05'),
-(18, 2, 20255472, 'Isabelle', 'Rebecca Briggs', 'Jones', '0', 'tywuly@mailinator.com', 750, '+639917137979', 63, '2003-05-17', 'Perferendis sunt sit', 'Annulled', 'Eum ea id saepe adi', 435, 557, 191, 'Hiram Hickman', '2025-11-05', '2025-11-05'),
-(19, 2, 20258055, 'Nadine', 'Erin Morgan', 'Richards', '0', 'jyrydo@mailinator.com', 362, '+639917137979', 34, '1997-10-10', 'Velit dolorem quos r', 'Widowed', 'Anim architecto veni', 239, 264, 513, 'Salvador Sellers', '2025-11-05', '2025-11-05'),
-(21, 2, 20254331, 'Maxine', 'McKenzie Villarreal', 'Levy', '0', 'sysedib@mailinator.com', 388, '+639917137979', 52, '1979-04-08', 'Et voluptatem Conse', 'Annulled', 'Enim molestiae ullam', 601, 713, 394, 'Scott Holman', '2025-11-05', '2025-11-05'),
-(25, 2, 20255137, 'Kylynn', 'Marshall Blackwell', 'Mcgowan', 'Cedric Briggs', 'lumyve@mailinator.com', 0, '+639917137979', 43, '1983-06-11', 'Non voluptatem archi', 'Single', 'Est laudantium veri', 666, 890, 560, 'Remedios Parks', '2025-11-05', '2025-11-05'),
-(26, 2, 20257915, 'Thaddeus', 'Malcolm King', 'Austin', 'Ivor Harvey', 'xetasuwobe@mailinator.com', 0, '+639917137979', 54, '1982-09-30', 'Dolor ut blanditiis ', 'Single', 'Voluptatibus fugiat ', 353, 496, 430, 'Nathan Curtis', '2025-11-05', '2025-11-05'),
-(29, 6, 20258319, 'Simon', 'Raya Avery', 'Johnston', 'Mira Vaughn', 'botur@mailinator.com', 330, '+639+1 (896) 606-4244', 26, '1980-03-07', 'Nisi nulla maiores e', 'married', NULL, 487, 275, 4, 'Bruce Huber', '2025-11-05', '2025-11-05'),
-(31, 6, 20257130, 'Wallace', 'Tanek Holcomb', 'Rodgers', 'Rose Cohen', 'tyqox@mailinator.com', 950, '+639+1 (311) 914-2143', 35, '2003-04-06', 'Aperiam ut aut hic d', 'divorced', NULL, 665, 159, 2, 'Bo Cabrera', '2025-11-05', '2025-11-05');
+INSERT INTO `users` (`id`, `role_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `suffix`, `email_address`, `hoa_number`, `phone_number`, `age`, `date_of_birth`, `citizenship`, `civil_status`, `account_status`, `home_address`, `lot_number`, `block_number`, `phase_number`, `village_name`, `date_created`, `date_updated`) VALUES
+(16, 2, 20257108, 'Isabelle', 'Rebecca Briggs', 'Jones', '0', 'tywuly@mailinator.com', 750, '+639917137979', 63, '2003-05-17', 'Perferendis sunt sit', 'Annulled', 0, 'Eum ea id saepe adi', 435, 557, 191, 'Hiram Hickman', '2025-11-05', '2025-11-05'),
+(18, 2, 20255472, 'Isabelle', 'Rebecca Briggs', 'Jones', '0', 'tywuly@mailinator.com', 750, '+639917137979', 63, '2003-05-17', 'Perferendis sunt sit', 'Annulled', 0, 'Eum ea id saepe adi', 435, 557, 191, 'Hiram Hickman', '2025-11-05', '2025-11-05'),
+(19, 2, 20258055, 'Nadine', 'Erin Morgan', 'Richards', '0', 'jyrydo@mailinator.com', 362, '+639917137979', 34, '1997-10-10', 'Velit dolorem quos r', 'Widowed', 0, 'Anim architecto veni', 239, 264, 513, 'Salvador Sellers', '2025-11-05', '2025-11-05'),
+(21, 2, 20254331, 'Maxine', 'McKenzie Villarreal', 'Levy', '0', 'sysedib@mailinator.com', 388, '+639917137979', 52, '1979-04-08', 'Et voluptatem Conse', 'Annulled', 0, 'Enim molestiae ullam', 601, 713, 394, 'Scott Holman', '2025-11-05', '2025-11-05'),
+(25, 2, 20255137, 'Kylynn', 'Marshall Blackwell', 'Mcgowan', 'Cedric Briggs', 'lumyve@mailinator.com', 0, '+639917137979', 43, '1983-06-11', 'Non voluptatem archi', 'Single', 0, 'Est laudantium veri', 666, 890, 560, 'Remedios Parks', '2025-11-05', '2025-11-05'),
+(26, 2, 20257915, 'Thaddeus', 'Malcolm King', 'Austin', 'Ivor Harvey', 'xetasuwobe@mailinator.com', 0, '+639917137979', 54, '1982-09-30', 'Dolor ut blanditiis ', 'Single', 0, 'Voluptatibus fugiat ', 353, 496, 430, 'Nathan Curtis', '2025-11-05', '2025-11-05'),
+(29, 6, 20258319, 'Simon', 'Raya Avery', 'Johnston', 'Mira Vaughn', 'botur@mailinator.com', 330, '+639+1 (896) 606-4244', 26, '1980-03-07', 'Nisi nulla maiores e', 'married', 0, NULL, 487, 275, 4, 'Bruce Huber', '2025-11-05', '2025-11-05'),
+(31, 4, 20257130, 'Zeus', 'Sierra', 'Fatima', 'Hammett', 'rixefudobi@mailinator.com', 0, '+639Alfreda', 199, '2025-01-19', 'Maris', 'Annulled', 2, 'Axel', 0, 0, 4, 'Jacob', '2025-11-05', '2025-11-05'),
+(32, 3, 20251311, 'Tanner', 'Vladimir Love', 'Flowers', 'Ivor Guerrero', 'jalilikiwo@mailinator.com', 0, '+639171379796', 64, '2009-12-29', 'Culpa ut placeat q', 'Divorced', 1, 'Ipsam aliquip expedi', 3, 177, 275, 'Wilma Edwards', '2025-11-05', '2025-11-05'),
+(33, 6, 202510852, 'Elizabeth', 'Timothy Moody', 'Hernandez', 'Victoria Cook', 'bexy@mailinator.com', 188, '+639+1 (375) 133-8027', 18, '2009-09-28', 'Omnis voluptatum cup', '1', 0, NULL, 83, 781, 4, 'Kasper Mcgowan', '2025-11-05', '2025-11-05'),
+(34, 6, 20257767, 'Charles', 'Colt Phillips', 'Wagner', 'Mikayla Baker', 'gizaqa@mailinator.com', 99, '+639+1 (543) 218-8555', 18, '1982-06-23', 'Quaerat in enim omni', '2', 0, NULL, 645, 540, 2, 'Hasad Griffin', '2025-11-05', '2025-11-05'),
+(35, 6, 202510583, 'Tamara', 'Signe Contreras', 'Bray', 'Quinlan Jones', 'dubisu@mailinator.com', 481, '+639+1 (122) 766-8358', 100, '1972-08-23', 'Nemo sed in recusand', '2', 0, NULL, 692, 943, 1, 'Cheryl Fisher', '2025-11-05', '2025-11-05'),
+(36, 6, 20253956, 'Amir', 'Natalie Byrd', 'Rosa', 'Kareem Thompson', 'powapoji@mailinator.com', 98, '+639+1 (148) 238-6787', 91, '1994-06-16', 'Quis odit culpa sed ', 'widowed', 1, NULL, 684, 63, 2, 'Pascale Mosley', '2025-11-05', '2025-11-05'),
+(37, 6, 20253308, 'mathew', 'Althea Carter', 'Vega', 'Darryl Moss', 'xobyhuwiju@mailinator.com', 678, '+6391 (955) 232-6483', 21, '2014-05-05', 'Iste quia rerum anim', 'Single', 1, '', 874, 564, 2, 'Hilda Strong', '2025-11-05', '2025-11-05'),
+(38, 6, 20257587, 'mathew', 'Althea Carter', 'Vega', 'Darryl Moss', 'xobyhuwiju@mailinator.com', 678, '+6391 (955) 232-6483', 21, '2014-05-05', 'Iste quia rerum anim', 'Married', 1, '', 874, 564, 2, 'Hilda Strong', '2025-11-05', '2025-11-05');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `fee_type`
+--
+ALTER TABLE `fee_type`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `roles`
@@ -111,6 +145,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `fee_type`
+--
+ALTER TABLE `fee_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
@@ -120,7 +160,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Constraints for dumped tables
