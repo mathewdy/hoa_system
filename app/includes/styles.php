a0991bdb03df673b843f99300314f126885d5c16
Ryan121716 <ryantecling@gmail.com>
2025-11-08 18:26:00 +0800

    Styles separated

---
+++ app/includes/styles.php
@@ -0,0 +1,12 @@
+<?php 
+  include_once ($_SERVER['DOCUMENT_ROOT'] . '/hoa_system/config.php');
+
+  echo 
+  '
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/flowbite@latest/dist/flowbite.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css" rel="stylesheet" />
+    <link href="'. BASE_PATH .'/assets/css/landing-page.css" rel="stylesheet" />
+  ';
+?>
