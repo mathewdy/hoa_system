ce582349e106c39dd8019fc9ee742206fa0b1a96
mathewdy <88813049+mathewdy@users.noreply.github.com>
2025-11-29 14:47:28 +0800

    Update feed.php

--- pages/news/feed.php
+++ pages/news/feed.php
@@ -17,15 +17,12 @@ $role = $_SESSION['role'];
 
     <?php
 
-    if($role =3 || $role ==3 ){
+    if($role == 3 || $role == 4 ){
         ?>
-
         <button onclick="openModal()" 
         class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
             Create Post
         </button>
-
-
         <?php
     }
 
